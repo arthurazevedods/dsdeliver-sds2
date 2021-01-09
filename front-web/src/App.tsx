19ad59855ad3031706dfc05064251de6c592368b
Arthur S <arthurazevedods@gmail.com>
2021-01-09 17:41:45 -0300

    Fix never used import

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,17 @@
+import { ToastContainer  } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
 import Routes from './Routes';
 
 
 
 function App() {
   return (
-  
-    <Routes />
-  
+    <>
+      <Routes />
+      <ToastContainer />
+    </>
   );
 }
 
